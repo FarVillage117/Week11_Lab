5ec1e57e49d71f409e0737385823dbfd3eaa85cf
FarVillage117 <143549383+FarVillage117@users.noreply.github.com>
2025-04-16 10:58:42 -0400

    8.6 Finishing the Drawer Challenge Progress

--- 314-W11/Assets/Scripts/Interactables/DrawerInteractable (1).cs
+++ 314-W11/Assets/Scripts/Interactables/DrawerInteractable (1).cs
@@ -3,12 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.Events;
 public class DrawerInteractable : XRGrabInteractable
 {
+    public UnityEvent OnDrawerDetach;
     [SerializeField] Transform drawerTransform;
     [SerializeField] XRSocketInteractor keySocket;
     public XRSocketInteractor GetKeySocket => keySocket;
     [SerializeField] XrPhysicsButtonInteractable physicsButton;
+    public XrPhysicsButtonInteractable GetPhysicsButton => physicsButton;
     [SerializeField] GameObject keyIndicatorLight;
     [SerializeField] bool isLocked;
     [SerializeField] bool isDetachable;
@@ -137,6 +140,7 @@ public class DrawerInteractable : XRGrabInteractable
     {
         isDetached = true;
         drawerTransform.SetParent(this.transform);
+        OnDrawerDetach?.Invoke();
     }
     private void ChangeLayerMask(string mask)
     {
